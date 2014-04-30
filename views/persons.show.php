a0fb0088882da6beb93caca566d49d0d5026e51b
Aymeric Assier <aymeric.assier@infomaniak.ch>
2014-04-30 15:53:05 +0200

    Improve right box design

--- views/persons.show.php
+++ views/persons.show.php
@@ -3,82 +3,41 @@
     <a href="#" class="close"><i class="fa fa-times"></i></a>
 </header>
 
-<div class="line head">
+<main>
 
-    <img src="http://placehold.it/200x200" alt=""/>
+    <div class="head">
 
-    <label for="firstnames">Prénom(s)</label>
-    <input type="text" id="firstnames" name="firstnames" value="<?= $person->firstnames ?>"/>
+        <img src="http://placehold.it/160x160" alt=""/>
 
-    <label for="lastname">Nom</label>
-    <input type="text" id="lastname" name="lastname" value="<?= $person->lastname ?>"/>
+        <div class="names">
+            <input type="text" name="firstnames" placeholder="Prénom(s)" value="<?= $person->firstnames ?>"/>
+            <input type="text" name="lastname" placeholder="Nom de famille" value="<?= $person->lastname ?>"/>
+        </div>
 
-    <div class="gender">
-        <label for="m">
-            <i class="fa fa-male"></i>
-        </label> <input type="radio" name="gender" id="m"/>
-        <label for="f">
-            <i class="fa fa-female"></i>
-        </label> <input type="radio" name="gender" id="f"/>
     </div>
 
-    <div class="clear"></div>
-
-</div>
-
-<div class="line event">
-
-    <div class="block">
-        <label for="event[0][name]">Evénement</label>
-        <input type="text" name="event[0][name]" id="event[0][name]" value="Naissance"/>
+    <div class="event">
+        <input type="text" name="event[0][name]" placeholder="Evénement" class="event-name"/>
+        le
+        <input type="text" name="event[0][date]" placeholder="Date" class="event-date"/>
+        à
+        <input type="text" name="event[0][name]" placeholder="Lieu" class="event-place"/>
     </div>
 
-    le
-
-    <div class="block">
-        <label for="event[0][date]">Date</label>
-        <input type="text" name="event[0][date]" id="event[0][date]" value="5 février 1989"/>
+    <div class="event">
+        <input type="text" name="event[0][name]" placeholder="Evénement" class="event-name"/>
+        le
+        <input type="text" name="event[0][date]" placeholder="Date" class="event-date"/>
+        à
+        <input type="text" name="event[0][name]" placeholder="Lieu" class="event-place"/>
     </div>
 
-    à
-
-    <div class="block">
-        <label for="event[0][place]">Lieu</label>
-        <input type="text" name="event[0][place]" id="event[0][place]" value="Evian"/>
+    <div class="note">
+        <textarea name="notes" placeholder="Notes diverses"></textarea>
     </div>
 
-    <!-- new line -->
+</main>
 
-    <div class="block">
-        <label for="event[1][name]">Evénement</label>
-        <input type="text" name="event[1][name]" id="event[1][name]"/>
-    </div>
-
-    le
-
-    <div class="block">
-        <label for="event[1][date]">Date</label>
-        <input type="text" name="event[1][date]" id="event[1][date]"/>
-    </div>
-
-    ?
-
-    <div class="block">
-        <label for="event[1][place]">Lieu</label>
-        <input type="text" name="event[1][place]" id="event[1][place]"/>
-    </div>
-
-    <div class="common">Naissance, bapê?me, diplôme, décès</div>
-
-</div>
-
-<div class="line">
-
-    <label for="notes">Notes</label>
-    <textarea name="notes" id="notes"></textarea>
-
-</div>
-
-<div class="line action">
+<footer>
     <a class="btn btn-primary" href="#">Sauvegarder</a>
-</div>
+</footer>
